b874ebb774234650c8efe76aaf8f3f50e8170205
unknown <apurbahasanj@gmail.com>
2023-02-27 22:14:55 +0600

    phone modal details done

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ const displayPhones = (phones, dataLimit) => {
                         <img src="${phone.image}" class="card-img-top" alt="...">
                         <div class="card-body">
                           <h5 class="card-title">${phone.phone_name}</h5>
-                          <p class="card-text text-secondary">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                          <p class="card-text text-secondary">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                          <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-secondary " data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details </Button>
                         </div>
                       </div>
         `;
@@ -62,6 +63,16 @@ document.getElementById('btn-search').addEventListener('click', function(){
 
 });
 
+// search input field enter key handler
+document.getElementById("search-field").addEventListener("keypress", function(e) {
+  console.log(e.key)
+    if (e.key === "Enter") {
+        // start loader
+  searchProcess(10)
+    }
+});
+
+
 const toggleSpinner = isLoading => {
   const loaderSection = document.getElementById('loader');
   if(isLoading){
@@ -76,6 +87,43 @@ const toggleSpinner = isLoading => {
 // Show all products
 document.getElementById('show-more-btn').addEventListener('click', function(){
   searchProcess()
-})
+});
 
-  loadPhones('iphone')
+// Load phone details
+const loadPhoneDetails = async id =>{
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhoneDetail(data.data)
+}
+
+const displayPhoneDetail = phone => {
+  console.log(phone)
+  const modalTitle = document.getElementById('phoneDetailModalLabel');
+  modalTitle.innerText = phone.name;
+
+  const phoneStorage = document.getElementById('phone-storage');
+  phoneStorage.innerText = phone.mainFeatures.storage;
+
+  const phoneDisplay = document.getElementById('displaySize');
+  phoneDisplay.innerText = phone.mainFeatures.displaySize;
+
+  const phoneChipSet = document.getElementById('chipSet');
+  phoneChipSet.innerText = phone.mainFeatures.chipSet;
+
+  const phoneMemory = document.getElementById('phone-memory');
+  phoneMemory.innerText = phone.mainFeatures.memory;
+
+  const wlan = document.getElementById('wlan');
+  wlan.innerText = phone.others.WLAN;
+
+  const bluetooth = document.getElementById('bluetooth');
+  bluetooth.innerText = phone.others.Bluetooth;
+
+  const gps = document.getElementById('gps');
+  gps.innerText = phone.others.GPS;
+
+  const releaseDate = document.getElementById('release-date');
+  releaseDate.innerText = phone.releaseDate;
+}
+  loadPhones('iphone', 10)
